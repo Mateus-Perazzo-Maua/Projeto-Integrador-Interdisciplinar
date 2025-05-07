e5ef885cfb1b70447183bab487647ded86ec227c
25.00357-5@maua.br <25.00357-5@maua.br>
2025-05-07 11:36:33 -0300

    bd atualização

--- BD_projetointegrador.sql
+++ BD_projetointegrador.sql
@@ -6,3 +6,10 @@ DROP TABLE tb_usuario;
 CREATE TABLE tb_usuario (id INT PRIMARY KEY AUTO_INCREMENT, nome VARCHAR(200), senha VARCHAR(200), tipo VARCHAR(200));
 INSERT INTO tb_usuario(nome, senha, tipo) VALUES ('aluno','aluno', 'aluno');
 INSERT INTO tb_usuario(nome, senha, tipo) VALUES ('professor','professor', 'professor');
+
+CREATE TABLE tb_materia(
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(200) NOT NULL
+);
+
+SELECT * FROM tb_materia;
