df20113379726b4ff48b1df147a8c3f3e1d7e920
25.00357-5@maua.br <25.00357-5@maua.br>
2025-05-07 09:33:37 -0300

    Banco de Dados

---
+++ BD_projetointegrador.sql
@@ -0,0 +1,8 @@
+CREATE SCHEMA projeto_integrador;
+
+USE projeto_integrador;
+
+DROP TABLE tb_usuario;
+CREATE TABLE tb_usuario (id INT PRIMARY KEY AUTO_INCREMENT, nome VARCHAR(200), senha VARCHAR(200), tipo VARCHAR(200));
+INSERT INTO tb_usuario(nome, senha, tipo) VALUES ('aluno','aluno', 'aluno');
+INSERT INTO tb_usuario(nome, senha, tipo) VALUES ('professor','professor', 'professor');
